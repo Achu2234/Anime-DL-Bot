2d526882ec7390508f664976eeb9081b23525435
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-05-16 19:22:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 # Encoding = 'utf-8'
 # Licensed under MIT License
 # Special Thanks for gogoanime
+import os
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = "3938951"
-api_hash = "6561686ba611b2d46efedef7debd6fa5"
-bot_token = "1892552434:AAHbBKM10Tjd0M_wiZNc9EGquVuRUQo-doA"
+api_id = os.environ.get("api_id", None)
+api_hash = os.environ.get("api_hash", None)
+bot_token = os.environ.get("bot_token", None)
